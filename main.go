f8afcb6b6c30cadc63e1edd8bebdcc90c2cbff41
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-12-30 09:40:02 +0100

    send body to the request

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"net/http"
 	u "net/url"
@@ -14,27 +15,28 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-func makeRequest(method string, url string, header_container *fyne.Container) string {
-	req, err := http.NewRequest(method, url, nil)
+func makeRequest(channel chan string, method string, url string, header_container *fyne.Container, body string) {
+	data := []byte(body)
+	req, err := http.NewRequest(method, url, bytes.NewReader(data))
 	if err != nil {
-		return ""
+		channel <- ""
 	} else {
 		for i := 0; i < len(header_container.Objects); i++ {
-            header_border := header_container.Objects[i].(*fyne.Container)
-            header_grid := header_border.Objects[0].(*fyne.Container)
+			header_border := header_container.Objects[i].(*fyne.Container)
+			header_grid := header_border.Objects[0].(*fyne.Container)
 			enabled := header_border.Objects[1].(*widget.Check)
-            name := header_grid.Objects[0].(*widget.Entry).Text
-            value := header_grid.Objects[1].(*widget.Entry).Text
+			name := header_grid.Objects[0].(*widget.Entry).Text
+			value := header_grid.Objects[1].(*widget.Entry).Text
 			regexp, _ := regexp.Compile(`^[A-Za-z[\]{}()<>\/@?=:";,-]*$`)
-            if enabled.Checked && name != "" && regexp.MatchString(name) && value != "" {
-                req.Header.Add(name, value)
-            }
-        }
+			if enabled.Checked && name != "" && regexp.MatchString(name) && value != "" {
+				req.Header.Add(name, value)
+			}
+		}
 		c := &http.Client{}
 		res, _ := c.Do(req)
 		bytes, _ := io.ReadAll(res.Body)
-		body := string(bytes)
-		return body
+		resbody := string(bytes)
+		channel <- resbody
 	}
 }
 
@@ -51,51 +53,88 @@ func main() {
 	response := widget.NewLabel("")
 	response.Wrapping = fyne.TextWrapWord
 	enabled := widget.NewCheck("", nil)
-	enabled.SetChecked(true)
 	header_name := widget.NewEntry()
 	header_name.SetPlaceHolder("name")
 	header_value := widget.NewEntry()
 	header_value.SetPlaceHolder("value")
+	header_name.OnChanged = func(s string) {
+		if len(s) == 0 || len(header_value.Text) == 0 {
+			enabled.SetChecked(false)
+		} else {
+			enabled.SetChecked(true)
+		}
+	}
+	header_value.OnChanged = func(s string) {
+		if len(s) == 0 || len(header_name.Text) == 0 {
+			enabled.SetChecked(false)
+		} else {
+			enabled.SetChecked(true)
+		}
+	}
 	header_grid := container.NewGridWithColumns(2, header_name, header_value)
 	header_border := container.NewBorder(nil, nil, enabled, nil, header_grid)
 	header_box := container.NewVBox(header_border)
 	tap := 0
 	plus := widget.NewButton("+", func() {
+		newEnabled := widget.NewCheck("", nil)
 		newHeader_name := widget.NewEntry()
 		newHeader_name.SetPlaceHolder("name")
 		newHeader_value := widget.NewEntry()
 		newHeader_value.SetPlaceHolder("value")
+		newHeader_name.OnChanged = func(s string) {
+			if len(s) == 0 || len(newHeader_value.Text) == 0 {
+				newEnabled.SetChecked(false)
+			} else {
+				newEnabled.SetChecked(true)
+			}
+		}
+		newHeader_value.OnChanged = func(s string) {
+			if len(s) == 0 || len(newHeader_value.Text) == 0 {
+				newEnabled.SetChecked(false)
+			} else {
+				newEnabled.SetChecked(true)
+			}
+		}
 		header_grid = container.NewGridWithColumns(2, newHeader_name, newHeader_value)
-		header_border = container.NewBorder(nil, nil, enabled, nil, header_grid)
+		header_border = container.NewBorder(nil, nil, newEnabled, nil, header_grid)
 		header_box.Add(header_border)
 		tap -= 1
 	})
 	minus := widget.NewButton("-", func() {
-		if len(header_box.Objects) - 1 > tap {
+		if len(header_box.Objects)-1 > tap {
 			tap += 1
 			header_box.Remove(header_box.Objects[-tap])
 		}
 	})
-	options := container.NewAppTabs(container.NewTabItem("Headers", container.NewScroll(container.NewVBox(header_box, plus, minus))), container.NewTabItem("Body", widget.NewMultiLineEntry()))
+	reqbody := widget.NewMultiLineEntry()
+	options := container.NewAppTabs(container.NewTabItem("Headers", container.NewScroll(container.NewVBox(header_box, plus, minus))), container.NewTabItem("Body", reqbody))
 	scroll_response := container.NewScroll(response)
 	send.OnTapped = func() {
+		channel := make(chan string)
+		var body string
 		if len(url.Text) == 0 {
 			_, err := u.ParseRequestURI(url.PlaceHolder)
 			if err == nil {
-				response.SetText(makeRequest(method.Selected, url.PlaceHolder, header_box))
+				go makeRequest(channel, method.Selected, url.PlaceHolder, header_box, reqbody.Text)
+				body = <-channel
+				response.SetText(body)
 			}
 		} else {
-			_, err := u.ParseRequestURI(url.PlaceHolder)
+			URL, err := u.ParseRequestURI(url.Text)
 			if err == nil {
-				if strings.HasSuffix(strings.ToLower(url.Text), ".png") || strings.HasSuffix(strings.ToLower(url.Text), ".jpg") || strings.HasSuffix(strings.ToLower(url.Text), ".jpeg") {
-					response.Hide()
-					img, _ := fyne.LoadResourceFromURLString(url.Text)
-					img_box := canvas.NewImageFromResource(img)
-					img_box.FillMode = canvas.ImageFillContain
-					scroll_response.Content = img_box
-					scroll_response.Refresh()
-				} else {
-					response.SetText(makeRequest(method.Selected, url.Text, header_box))
+				if URL.Hostname() != "" {
+					if strings.HasSuffix(strings.ToLower(url.Text), ".png") || strings.HasSuffix(strings.ToLower(url.Text), ".jpg") || strings.HasSuffix(strings.ToLower(url.Text), ".jpeg") {
+						response.Hide()
+						img, _ := fyne.LoadResourceFromURLString(url.Text)
+						img_box := canvas.NewImageFromResource(img)
+						img_box.FillMode = canvas.ImageFillContain
+						scroll_response.Content = img_box
+						scroll_response.Refresh()
+					} else {
+						go makeRequest(channel, method.Selected, url.Text, header_box, reqbody.Text)
+						body = <-channel
+						response.SetText(body)
+					}
 				}
 			}
 		}
