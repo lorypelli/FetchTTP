89ba691a81bb152a1f020326f12b521f0a1ae2ab
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-01 09:46:22 +0100

    continuing websockets

--- main.go
+++ main.go
@@ -66,32 +66,38 @@ func main() {
 	url_ws.SetPlaceHolder("echo.zuplo.io")
 	send := widget.NewButton("Send", nil)
 	connect := widget.NewButton("Connect", nil)
-	response := widget.NewLabel("")
-	response.Wrapping = fyne.TextWrapWord
+	http_response := widget.NewLabel("")
+	http_response.Wrapping = fyne.TextWrapWord
+	ws_response := widget.NewLabel("")
+	ws_response.Wrapping = fyne.TextWrapWord
 	enabled := widget.NewCheck("", nil)
-	header_name := widget.NewEntry()
-	header_name.SetPlaceHolder("name")
-	header_value := widget.NewEntry()
-	header_value.SetPlaceHolder("value")
-	header_name.OnChanged = func(s string) {
-		if len(s) == 0 || len(header_value.Text) == 0 {
+	http_header_name := widget.NewEntry()
+	http_header_name.SetPlaceHolder("name")
+	http_header_value := widget.NewEntry()
+	http_header_value.SetPlaceHolder("value")
+	ws_header_name := widget.NewEntry()
+	ws_header_name.SetPlaceHolder("name")
+	ws_header_value := widget.NewEntry()
+	ws_header_value.SetPlaceHolder("value")
+	http_header_name.OnChanged = func(s string) {
+		if len(s) == 0 || len(http_header_value.Text) == 0 {
 			enabled.SetChecked(false)
 		} else {
 			enabled.SetChecked(true)
 		}
 	}
-	header_value.OnChanged = func(s string) {
-		if len(s) == 0 || len(header_name.Text) == 0 {
+	http_header_value.OnChanged = func(s string) {
+		if len(s) == 0 || len(http_header_name.Text) == 0 {
 			enabled.SetChecked(false)
 		} else {
 			enabled.SetChecked(true)
 		}
 	}
-	header_grid := container.NewGridWithColumns(2, header_name, header_value)
-	header_border := container.NewBorder(nil, nil, enabled, nil, header_grid)
-	header_box := container.NewVBox(header_border)
-	tap := 0
-	plus := widget.NewButton("+", func() {
+	http_header_grid := container.NewGridWithColumns(2, http_header_name, http_header_value)
+	http_header_border := container.NewBorder(nil, nil, enabled, nil, http_header_grid)
+	http_header_box := container.NewVBox(http_header_border)
+	tap_http := 0
+	plus_http := widget.NewButton("+", func() {
 		newEnabled := widget.NewCheck("", nil)
 		newHeader_name := widget.NewEntry()
 		newHeader_name.SetPlaceHolder("name")
@@ -111,40 +117,40 @@ func main() {
 				newEnabled.SetChecked(true)
 			}
 		}
-		header_grid = container.NewGridWithColumns(2, newHeader_name, newHeader_value)
-		header_border = container.NewBorder(nil, nil, newEnabled, nil, header_grid)
-		header_box.Add(header_border)
-		tap -= 1
+		http_header_grid = container.NewGridWithColumns(2, newHeader_name, newHeader_value)
+		http_header_border = container.NewBorder(nil, nil, newEnabled, nil, http_header_grid)
+		http_header_box.Add(http_header_border)
+		tap_http -= 1
 	})
-	minus := widget.NewButton("-", func() {
-		if len(header_box.Objects)-1 > tap {
-			tap += 1
-			header_box.Remove(header_box.Objects[-tap+1])
+	minus_http := widget.NewButton("-", func() {
+		if len(http_header_box.Objects)-1 > tap_http {
+			tap_http += 1
+			http_header_box.Remove(http_header_box.Objects[-tap_http+1])
 		}
 	})
 	reqbody := widget.NewMultiLineEntry()
-	options := container.NewAppTabs(container.NewTabItem("Headers", container.NewScroll(container.NewVBox(header_box, plus, minus))), container.NewTabItem("Body", reqbody))
-	response_headers := container.NewVBox()
-	scroll_response := container.NewScroll(response)
-	response_options := container.NewAppTabs(container.NewTabItem("Headers", container.NewScroll(response_headers)), container.NewTabItem("Response", scroll_response))
-	response_status := widget.NewLabel("")
+	http_options := container.NewAppTabs(container.NewTabItem("Headers", container.NewScroll(container.NewVBox(http_header_box, plus_http, minus_http))), container.NewTabItem("Body", reqbody))
+	http_response_headers := container.NewVBox()
+	scroll_http_response := container.NewScroll(http_response)
+	http_response_options := container.NewAppTabs(container.NewTabItem("Headers", container.NewScroll(http_response_headers)), container.NewTabItem("Response", scroll_http_response))
+	http_response_status := widget.NewLabel("")
 	send.OnTapped = func() {
-		response_headers.RemoveAll()
-		response_options.SelectIndex(1)
+		http_response_headers.RemoveAll()
+		http_response_options.SelectIndex(1)
 		if len(url_http.Text) == 0 {
 			urlWithHTTPS := fmt.Sprintf("https://%s", url_http.PlaceHolder)
 			_, err := u.ParseRequestURI(urlWithHTTPS)
 			if err == nil {
-				status, headers, body := makeRequest(method.Selected, urlWithHTTPS, header_box, reqbody.Text)
-				response_status.SetText(fmt.Sprint(status))
-				response_status.Refresh()
+				status, headers, body := makeRequest(method.Selected, urlWithHTTPS, http_header_box, reqbody.Text)
+				http_response_status.SetText(fmt.Sprint(status))
+				http_response_status.Refresh()
 				for k, v := range headers {
 					str, _ := j.Marshal(v)
 					response_header := widget.NewLabel(fmt.Sprintf("%s: %s", k, str))
 					response_header.Wrapping = fyne.TextWrapWord
-					response_headers.Add(response_header)
+					http_response_headers.Add(response_header)
 				}
-				response.SetText(string(body))
+				http_response.SetText(string(body))
 			}
 		} else {
 			urlWithHTTPS := url_http.Text
@@ -153,9 +159,9 @@ func main() {
 			}
 			_, err := u.ParseRequestURI(urlWithHTTPS)
 			if err == nil {
-				status, headers, body := makeRequest(method.Selected, urlWithHTTPS, header_box, reqbody.Text)
-				response_status.SetText(fmt.Sprint(status))
-				response_status.Refresh()
+				status, headers, body := makeRequest(method.Selected, urlWithHTTPS, http_header_box, reqbody.Text)
+				http_response_status.SetText(fmt.Sprint(status))
+				http_response_status.Refresh()
 				isImage := false
 				for k, v := range headers {
 					str, _ := j.Marshal(v)
@@ -164,39 +170,92 @@ func main() {
 					}
 					response_header := widget.NewLabel(fmt.Sprintf("%s: %s", k, str))
 					response_header.Wrapping = fyne.TextWrapWord
-					response_headers.Add(response_header)
+					http_response_headers.Add(response_header)
 				}
 				if isImage {
-					response.Hide()
+					http_response.Hide()
 					img, _ := fyne.LoadResourceFromURLString(urlWithHTTPS)
 					img_box := canvas.NewImageFromResource(img)
 					img_box.FillMode = canvas.ImageFillContain
-					scroll_response.Content = img_box
-					scroll_response.Refresh()
+					scroll_http_response.Content = img_box
+					scroll_http_response.Refresh()
 				} else {
-					response.SetText(string(body))
+					http_response.SetText(string(body))
 				}
 			}
 		}
-		response.Refresh()
+		http_response.Refresh()
+	}
+	connect.OnTapped = func() {
+		url_ws.Disable()
+		http_response_headers.RemoveAll()
+		http_response_options.SelectIndex(1)
+		if len(url_http.Text) == 0 {
+			urlWithHTTPS := fmt.Sprintf("https://%s", url_http.PlaceHolder)
+			_, err := u.ParseRequestURI(urlWithHTTPS)
+			if err == nil {
+				status, headers, body := makeRequest(method.Selected, urlWithHTTPS, http_header_box, reqbody.Text)
+				http_response_status.SetText(fmt.Sprint(status))
+				http_response_status.Refresh()
+				for k, v := range headers {
+					str, _ := j.Marshal(v)
+					response_header := widget.NewLabel(fmt.Sprintf("%s: %s", k, str))
+					response_header.Wrapping = fyne.TextWrapWord
+					http_response_headers.Add(response_header)
+				}
+				http_response.SetText(string(body))
+			}
+		} else {
+			urlWithHTTPS := url_http.Text
+			if !strings.HasPrefix(strings.ToLower(urlWithHTTPS), "http") || !strings.HasPrefix(strings.ToLower(urlWithHTTPS), "https") {
+				urlWithHTTPS = fmt.Sprintf("https://%s", url_http.Text)
+			}
+			_, err := u.ParseRequestURI(urlWithHTTPS)
+			if err == nil {
+				status, headers, body := makeRequest(method.Selected, urlWithHTTPS, http_header_box, reqbody.Text)
+				http_response_status.SetText(fmt.Sprint(status))
+				http_response_status.Refresh()
+				isImage := false
+				for k, v := range headers {
+					str, _ := j.Marshal(v)
+					if strings.ToLower(k) == "content-type" && strings.Contains(strings.ToLower(string(str)), "image/png") || strings.Contains(strings.ToLower(string(str)), "image/jpeg") {
+						isImage = true
+					}
+					response_header := widget.NewLabel(fmt.Sprintf("%s: %s", k, str))
+					response_header.Wrapping = fyne.TextWrapWord
+					http_response_headers.Add(response_header)
+				}
+				if isImage {
+					http_response.Hide()
+					img, _ := fyne.LoadResourceFromURLString(urlWithHTTPS)
+					img_box := canvas.NewImageFromResource(img)
+					img_box.FillMode = canvas.ImageFillContain
+					scroll_http_response.Content = img_box
+					scroll_http_response.Refresh()
+				} else {
+					http_response.SetText(string(body))
+				}
+			}
+		}
+		http_response.Refresh()
 	}
 	url_http.OnSubmitted = func(_ string) {
-		response_headers.RemoveAll()
-		response_options.SelectIndex(1)
+		http_response_headers.RemoveAll()
+		http_response_options.SelectIndex(1)
 		if len(url_http.Text) == 0 {
 			urlWithHTTPS := fmt.Sprintf("https://%s", url_http.PlaceHolder)
 			_, err := u.ParseRequestURI(urlWithHTTPS)
 			if err == nil {
-				status, headers, body := makeRequest(method.Selected, urlWithHTTPS, header_box, reqbody.Text)
-				response_status.SetText(fmt.Sprint(status))
-				response_status.Refresh()
+				status, headers, body := makeRequest(method.Selected, urlWithHTTPS, http_header_box, reqbody.Text)
+				http_response_status.SetText(fmt.Sprint(status))
+				http_response_status.Refresh()
 				for k, v := range headers {
 					str, _ := j.Marshal(v)
 					response_header := widget.NewLabel(fmt.Sprintf("%s: %s", k, str))
 					response_header.Wrapping = fyne.TextWrapWord
-					response_headers.Add(response_header)
+					http_response_headers.Add(response_header)
 				}
-				response.SetText(string(body))
+				http_response.SetText(string(body))
 			}
 		} else {
 			urlWithHTTPS := url_http.Text
@@ -205,9 +264,9 @@ func main() {
 			}
 			_, err := u.ParseRequestURI(urlWithHTTPS)
 			if err == nil {
-				status, headers, body := makeRequest(method.Selected, urlWithHTTPS, header_box, reqbody.Text)
-				response_status.SetText(fmt.Sprint(status))
-				response_status.Refresh()
+				status, headers, body := makeRequest(method.Selected, urlWithHTTPS, http_header_box, reqbody.Text)
+				http_response_status.SetText(fmt.Sprint(status))
+				http_response_status.Refresh()
 				isImage := false
 				for k, v := range headers {
 					str, _ := j.Marshal(v)
@@ -216,24 +275,24 @@ func main() {
 					}
 					response_header := widget.NewLabel(fmt.Sprintf("%s: %s", k, str))
 					response_header.Wrapping = fyne.TextWrapWord
-					response_headers.Add(response_header)
+					http_response_headers.Add(response_header)
 				}
 				if isImage {
-					response.Hide()
+					http_response.Hide()
 					img, _ := fyne.LoadResourceFromURLString(urlWithHTTPS)
 					img_box := canvas.NewImageFromResource(img)
 					img_box.FillMode = canvas.ImageFillContain
-					scroll_response.Content = img_box
-					scroll_response.Refresh()
+					scroll_http_response.Content = img_box
+					scroll_http_response.Refresh()
 				} else {
-					response.SetText(string(body))
+					http_response.SetText(string(body))
 				}
 			}
 		}
-		response.Refresh()
+		http_response.Refresh()
 	}
-	http := container.NewBorder(container.NewBorder(nil, nil, method, send, container.NewBorder(nil, nil, nil, nil, url_http)), nil, nil, nil, container.NewHSplit(options, container.NewBorder(nil, nil, nil, response_status, response_options)))
-	ws := container.NewBorder(container.NewBorder(nil, nil, nil, connect, container.NewBorder(nil, nil, nil, nil, url_ws)), nil, nil, nil, container.NewHSplit(options, container.NewBorder(nil, nil, nil, response_status, response_options)))
+	http := container.NewBorder(container.NewBorder(nil, nil, method, send, container.NewBorder(nil, nil, nil, nil, url_http)), nil, nil, nil, container.NewHSplit(http_options, container.NewBorder(nil, nil, nil, http_response_status, http_response_options)))
+	ws := container.NewBorder(container.NewBorder(nil, nil, nil, connect, container.NewBorder(nil, nil, nil, nil, url_ws)), nil, nil, nil, container.NewHSplit(http_options, container.NewBorder(nil, nil, nil, http_response_status, http_response_options)))
 	tabs := container.NewAppTabs(container.NewTabItem("HTTP", http), container.NewTabItem("WS", ws))
 	tabs.SetTabLocation(container.TabLocationLeading)
 	program.SetContent(tabs)
