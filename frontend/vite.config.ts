aaf3519aff55789984dbd22d68a3db6ed4568070
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-02-26 07:16:35 +0100

    new background color and vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,31 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
-// https://vitejs.dev/config/
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 export default defineConfig({
-    plugins: [vue()]
+    plugins: [vue(), ViteMinifyPlugin()],
+    optimizeDeps: {
+        include: [
+            'vue'
+        ]
+    },
+    server: {
+        host: '127.0.0.1'
+    },
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                ecma: 2020,
+                passes: 10
+            }
+        },
+        rollupOptions: {
+            output: {
+                dir: './dist',
+                entryFileNames: 'index.js',
+                assetFileNames: 'style.css',
+                compact: true
+            }
+        }
+    }
 });
