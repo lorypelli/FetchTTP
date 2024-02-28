cafa91fc147db16dc28a48154c51ad6452b74b90
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-02-28 15:19:31 +0100

    started query and headers

--- frontend/src/components/Split.vue
+++ frontend/src/components/Split.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { ElTabPane, ElTabs } from 'element-plus';
+import { ElButton, ElCheckbox, ElInput, ElTabPane, ElTabs } from 'element-plus';
 import Splitter from 'primevue/splitter';
 import SplitterPanel from 'primevue/splitterpanel';
+import { ref } from 'vue';
 defineOptions({
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'Split',
@@ -9,26 +10,53 @@ defineOptions({
         Splitter,
         SplitterPanel,
         ElTabPane,
-        ElTabs
+        ElTabs,
+        ElInput,
+        ElButton,
+        ElCheckbox
     }
 });
 const props = defineProps<{
     type: 'http' | 'ws'
 }>();
+const headerName = ref('');
+const headerValue = ref('');
+const queryName = ref('');
+const queryValue = ref('');
 </script>
 <template>
     <Splitter>
         <SplitterPanel :min-size="20">
-            <ElTabs>
-                <ElTabPane label="Headers"></ElTabPane>
-                <ElTabPane label="Query"></ElTabPane>
+            <ElTabs class="pr-2">
+                <ElTabPane label="Headers">
+                    <div class="flex space-x-2 pr-2">
+                        <ElCheckbox />
+                        <ElInput v-model="headerName"></ElInput>
+                        <ElInput v-model="headerValue"></ElInput>
+                    </div>
+                    <div class="flex pt-2">
+                        <ElButton>+</ElButton>
+                        <ElButton>-</ElButton>
+                    </div>
+                </ElTabPane>
+                <ElTabPane label="Query">
+                    <div class="flex space-x-2 pr-2">
+                        <ElCheckbox />
+                        <ElInput v-model="queryName"></ElInput>
+                        <ElInput v-model="queryValue"></ElInput>
+                    </div>
+                    <div class="flex pt-2">
+                        <ElButton>+</ElButton>
+                        <ElButton>-</ElButton>
+                    </div>
+                </ElTabPane>
                 <ElTabPane label="Cookies"></ElTabPane>
                 <ElTabPane label="Body" v-if="props.type == 'http'"></ElTabPane>
                 <ElTabPane label="Message" v-if="props.type == 'ws'"></ElTabPane>
             </ElTabs>
         </SplitterPanel>
         <SplitterPanel :min-size="20">
-            <ElTabs>
+            <ElTabs class="pl-2">
                 <ElTabPane label="Headers"></ElTabPane>
                 <ElTabPane label="Response"></ElTabPane>
             </ElTabs>
